e1d791bda6257c6ef6996cdcfb34003cfd1b8591
Watanabe Takashi <wtnb75@gmail.com>
2024-12-19 22:04:01 +0900

    volumes (#3)

--- dlabel/main.py
+++ dlabel/main.py
@@ -57,6 +57,7 @@ def docker_option(func):
 @verbose_option
 @docker_option
 def summary(client: docker.DockerClient, output):
+    """show name and labels of containers"""
     res: list[dict] = []
     for ctn in client.containers.list():
         res.append({
@@ -71,6 +72,7 @@ def summary(client: docker.DockerClient, output):
 @verbose_option
 @docker_option
 def attrs(client: docker.DockerClient, output):
+    """show name and attributes of containers"""
     res: list[dict] = []
     for ctn in client.containers.list():
         res.append({
@@ -125,13 +127,44 @@ def convdict_differ(
             todict[v] = dict_ctn[k]
 
 
+def copy_files(ctn: docker.models.containers.Container, src: str, dst: str):
+    def tfilter(member, path):
+        res = tarfile.data_filter(member, path)
+        if res:
+            if '/' in res.name:
+                _, res.name = res.name.split('/', 1)
+                return res
+        return None
+
+    _log.info("copy %s:%s -> %s", ctn.name, src, dst)
+    odir = Path(dst)
+    bin, arc = ctn.get_archive(src)
+    _log.debug("arc=%s", arc)
+    bio = io.BytesIO()
+    for x in bin:
+        bio.write(x)
+    bio.seek(0)
+    tf = tarfile.TarFile(fileobj=bio)
+    members = tf.getmembers()
+    if len(members) == 1 and members[0].isreg():
+        _log.info("single file: %s", members[0])
+        tf.extractall(odir.parent, filter='data')
+    else:
+        odir.mkdir(exist_ok=True, parents=True)
+        tf.extractall(odir, filter=tfilter)
+    tf.close()
+    bio.close()
+
+
 @cli.command()
 @click.option("--output", type=click.Path(file_okay=False, dir_okay=True, exists=True, writable=True))
 @click.option("--all/--compose", default=False, show_default=True)
+@click.option("--volume/--no-volume", default=True, show_default=True)
 @click.option("--project", default="*", show_default=True)
 @verbose_option
 @docker_option
-def compose(client: docker.DockerClient, output, all, project):
+def compose(client: docker.DockerClient, output, all, project, volume):
+    """generate docker-compose.yml from running containers"""
     svcs = {}
     vols = {}
     nets = {}
@@ -142,12 +175,13 @@ def compose(client: docker.DockerClient, output, all, project):
         proj = labels.get("com.docker.compose.project")
         wdir = Path(labels.get("com.docker.compose.project.working_dir", "/"))
         if not all and not proj:
-            _log.debug("not --all: %s", ctn.name)
+            _log.debug("skip: no project, not --all: %s", ctn.name)
             continue
         if not all and proj and not fnmatch.fnmatch(proj, project):
             _log.debug("skip by project (%s)", proj)
             continue
         name = labels.get("com.docker.compose.service", ctn.name)
+        _log.info("processing %s, service=%s", ctn.name, name)
         img = ctn.image
         imglabel = img.labels
         imgconfig = img.attrs.get("Config", {})
@@ -174,33 +208,8 @@ def compose(client: docker.DockerClient, output, all, project):
                 cvols.append(f"{src}:{dest}")
             elif len(v) == 3:
                 cvols.append(f"{src}:{dest}:{v[2]}")
-            if output and isinstance(src, str) and src.startswith("./"):
-                def tfilter(member, path):
-                    res = tarfile.data_filter(member, path)
-                    if res:
-                        if '/' in res.name:
-                            _, res.name = res.name.split('/', 1)
-                            return res
-                    return None
-                _log.info("copy %s:%s -> %s", name, dest, src)
-                odir: Path = Path(output) / src
-                assert odir.is_relative_to(output)
-                bin, arc = ctn.get_archive(dest)
-                _log.debug("arc=%s", arc)
-                bio = io.BytesIO()
-                for x in bin:
-                    bio.write(x)
-                bio.seek(0)
-                tf = tarfile.TarFile(fileobj=bio)
-                members = tf.getmembers()
-                if len(members) == 1 and members[0].isreg():
-                    _log.info("single file: %s", members[0])
-                    tf.extractall(odir.parent, filter='data')
-                else:
-                    odir.mkdir(exist_ok=True, parents=True)
-                    tf.extractall(odir, filter=tfilter)
-                tf.close()
-                bio.close()
+            if output and volume and isinstance(src, str) and src.startswith("./"):
+                copy_files(ctn, dest, Path(output) / src)
             elif output:
                 _log.info("skip copy: %s:%s -> %s", name, dest, src)
         for m in hostconfig.get("Mounts", []):
@@ -299,6 +308,7 @@ def tflabel2dict(labels: dict[str, str], prefix: str) -> dict[str, dict[str, str
 @verbose_option
 @docker_option
 def traefik2nginx(client: docker.DockerClient, output, ipaddr):
+    """generate nginx configuration from traefik labels"""
     for ctn in client.containers.list():
         name = ctn.name
         labels = ctn.attrs["Config"]["Labels"]
@@ -367,6 +377,50 @@ def traefik2nginx(client: docker.DockerClient, output, ipaddr):
             for i in confs:
                 print("  "+i+";")
             print("}")
+
+
+@cli.command()
+@docker_option
+@verbose_option
+def list_volume(client: docker.DockerClient):
+    """list volumes"""
+    yaml.dump([x.attrs for x in client.volumes.list()], stream=sys.stdout,
+              allow_unicode=True, encoding="utf-8", sort_keys=False)
+
+
+@cli.command()
+@docker_option
+@verbose_option
+@click.option("--image", default='hello-world', show_default=True)
+@click.option("--output", type=click.File("wb"), default="-", show_default=True)
+@click.option("-z", is_flag=True, help="compress with gzip")
+@click.argument("volume")
+def tar_volume(client: docker.DockerClient, volume, image, output, z):
+    """get volume content as tar"""
+    mount = "/" + volume.strip("/")
+    vol = client.volumes.get(volume)
+    _log.debug("Volume %s found with ID %s", volume, vol.id)
+
+    try:
+        img = client.images.get(image)
+        _log.debug("Image %s found locally", image)
+    except docker.errors.ImageNotFound:
+        img = client.images.pull(image)
+        _log.debug("Image %s pulled successfully", image)
+
+    mnt = docker.types.Mount(target=mount, source=vol.id, read_only=True)
+    cl = client.containers.create(img, mounts=[mnt])
+    _log.debug("Container created with image %s and volume %s mounted at %s", image, volume, mount)
+
+    try:
+        bin, _ = cl.get_archive(mount, encode_stream=z)
+        _log.debug("Starting to archive volume %s", volume)
+        for b in bin:
+            output.write(b)
+        _log.debug("Volume %s archived successfully", volume)
+    finally:
+        cl.remove(force=True)
+        _log.debug("Container removed")
 
 
 if __name__ == "__main__":
