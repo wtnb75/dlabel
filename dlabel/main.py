1196eab27af16ff3dcfd9d1f157fcbbb64acf27d
Watanabe Takashi <wtnb75@gmail.com>
2024-12-21 20:58:06 +0900

    compress middleware (#5)

--- dlabel/main.py
+++ dlabel/main.py
@@ -335,6 +335,76 @@ def find_server_block(conf: dict, server_name: str) -> list:
                         return srv.get("block", [])
 
 
+def middleware2nginx(mdlconf: dict[str, str]) -> list[dict]:
+    res = []
+    del_prefix = ""
+    add_prefix = "/"
+    if mdlconf.get("stripprefix.prefixes"):
+        del_prefix = mdlconf["stripprefix.prefixes"]
+    elif mdlconf.get("stripprefixregex.regex"):
+        del_prefix = mdlconf.get("stripprefixregex.regex")
+    if mdlconf.get("addprefix.prefix"):
+        add_prefix = mdlconf["addprefix.prefix"]
+    if del_prefix or add_prefix != "/":
+        res.append({
+            "directive": "rewrite",
+            "args": [f"{del_prefix}(.*)", f"{add_prefix}$1", "break"],
+        })
+    for k, v in mdlconf.items():
+        if k.startswith("headers.customrequestheaders."):
+            hdr = k[len("headers.customrequestheaders."):]
+            res.append({
+                "directive": "proxy_set_header",
+                "args": [hdr, v],
+            })
+        elif k.startswith("headers.customresponseheaders."):
+            hdr = k[len("headers.customresponseheaders."):]
+            res.append({
+                "directive": "add_header",
+                "args": [hdr, v],
+            })
+        elif k.split(".", 1)[0] not in ("stripprefix", "addprefix", "headers"):
+            _log.info("not supported middleware: %s", k)
+            res.append({
+                "directive": "#",
+                "comment": f" not supported middleware: {k}",
+                "line": 1,
+            })
+    if mdlconf.get("compress"):
+        res.append({
+            "directive": "gzip",
+            "args": ["on"],
+        })
+        if mdlconf.get("compress.includedcontenttypes"):
+            res.append({
+                "directive": "gzip_types",
+                "args": [mdlconf["compress.includedcontenttypes"]],
+            })
+        if mdlconf.get("compress.minresponsebodybytes"):
+            res.append({
+                "directive": "gzip_min_length",
+                "args": [mdlconf["compress.minresponsebodybytes"]],
+            })
+    return res
+
+
+def get_middlewares(middlewares: dict[str, dict[str, str]], names: list[str], blk: list[dict]) -> dict:
+    mdlconf = {}
+    for idx, mname in enumerate(names, start=1):
+        if not mname:
+            continue
+        if mname not in middlewares:
+            _log.info("middleware not found: %s", mname)
+            blk.append({
+                "directive": "#",
+                "comment": f" middleware {mname} not found",
+                "line": idx,
+            })
+        _log.debug("middleware[%s]: %s", names, mname)
+        mdlconf.update(middlewares.get(mname, {}))
+    return mdlconf
+
+
 @cli.command()
 @click.option("--output", type=click.File("w"), default="-", show_default=True)
 @click.option("--baseconf", type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True), default=None, show_default=True)
@@ -402,53 +472,9 @@ http {server {server_name %s;}}
                 "args": [f"http://{pass_to}:{dest}"],
             })
             mdls = router_config.get("middlewares", "").split(",")
-            mdlconf = {}
-            for idx, mname in enumerate(mdls, start=1):
-                if not mname:
-                    continue
-                _log.debug("middleware[%s]: %s", location_key, mname)
-                if mname not in middlewares:
-                    _log.info("middleware not found: %s", mname)
-                    blk.append({
-                        "directive": "#",
-                        "comment": f" middleware {mname} not found",
-                        "line": idx,
-                    })
-                mdlconf.update(middlewares.get(mname, {}))
+            mdlconf = get_middlewares(middlewares, mdls, blk)
             _log.debug("middleware configurations: %s", mdlconf)
-            del_prefix = ""
-            add_prefix = "/"
-            if mdlconf.get("stripprefix.prefixes"):
-                del_prefix = mdlconf["stripprefix.prefixes"]
-            elif mdlconf.get("stripprefixregex.regex"):
-                del_prefix = mdlconf.get("stripprefixregex.regex")
-            if mdlconf.get("addprefix.prefix"):
-                add_prefix = mdlconf["addprefix.prefix"]
-            if del_prefix or add_prefix != "/":
-                blk.append({
-                    "directive": "rewrite",
-                    "args": [f"{del_prefix}(.*)", f"{add_prefix}$1", "break"],
-                })
-            for k, v in mdlconf.items():
-                if k.startswith("headers.customrequestheaders."):
-                    hdr = k[len("headers.customrequestheaders."):]
-                    blk.append({
-                        "directive": "proxy_set_header",
-                        "args": [hdr, v],
-                    })
-                elif k.startswith("headers.customresponseheaders."):
-                    hdr = k[len("headers.customresponseheaders."):]
-                    blk.append({
-                        "directive": "add_header",
-                        "args": [hdr, v],
-                    })
-                elif k.split(".", 1)[0] not in ("stripprefix", "addprefix", "headers"):
-                    _log.info("not supported middleware: %s", k)
-                    blk.append({
-                        "directive": "#",
-                        "comment": f" not supported middleware: {k}",
-                        "line": 1,
-                    })
+            blk.extend(middleware2nginx(mdlconf))
             target.append({
                 "directive": "#",
                 "comment": f" {name}: {' '.join(location_key)} -> {addresses[0]}:{dest}",
